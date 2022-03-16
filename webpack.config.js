a35ae7574e4b88198e5d7c453fc09dc07b3bdd4b
unclepete-20 <pedroparty310@gmail.com>
2022-03-15 20:57:35 -0600

    Se ha configurado Babel

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,8 @@ module.exports = {
                 use: {
                   loader: 'babel-loader',
                   options: {
-                    presets: ['@babel/preset-env']
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime']
                   }
                 }
               },
